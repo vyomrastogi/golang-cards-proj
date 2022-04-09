0b420397091423b2aeb663284156d1385543b121
Vyom <vyom.reloaded@gmail.com>
2022-04-09 15:18:54 -0400

    test to write/read from file and clean up after test

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 //tests if new deck is always 16 length
 func TestNewDeck(t *testing.T) {
@@ -8,4 +11,27 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+	if d[0] != "One of Spades" {
+		t.Errorf("Expected first card to be One of Spades, but got %v", d[0])
+	}
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected first card to be Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	//delete any ecisting test files
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(loadedDeck))
+	}
+
+	//cleanup after test
+	os.Remove("_decktesting")
 }
