e78020fbdf8b6232f94feed388fd7eb5b488a67a
Vyom <vyom.reloaded@gmail.com>
2022-04-12 00:00:07 -0400

    structs in go

--- struct/main.go
+++ struct/main.go
@@ -2,24 +2,37 @@ package main
 
 import "fmt"
 
+//embedded struct of contactInfo
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 //define a new custom type `person` whicn `extends` struct
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 
 	//create a new person
 	//go relies on order of fields to assign values to struct fields
-	tony := person{"Tony", "Stark"} // NOT RECOMMENDED
+	tony := person{"Tony", "Stark", contactInfo{}} // NOT RECOMMENDED
 
 	//another syntax to define struct
-	thor := person{firstName: "Thor", lastName: "Odinson"}
+	thor := person{firstName: "Thor", lastName: "Odinson", contact: contactInfo{
+		email: "thor@avengers.com", zipCode: 00000}}
 
-	fmt.Println(tony, thor)
+	fmt.Println(tony)
 	//OP: {Tony Stark} {Thor Odinson}
 
-	fmt.Printf("%+v", thor)
-	//OP: {firstName:Thor lastName:Odinson}
+	thor.print()
+	//OP: {firstName:Thor lastName:Odinson contact:{email:thor@avengers.com zipCode:0}}
+}
+
+//print person with reciever function
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
