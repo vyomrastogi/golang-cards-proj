aea44da35abcba27eadceffc97113a2abe111a0f
Vyom <vyom.reloaded@gmail.com>
2022-04-30 11:58:41 -0400

    how to declare maps

---
+++ maps/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//How to declare maps
+	//A map of string, string
+	colors := map[string]string{
+		"red":  "#FF0000",
+		"blue": "#0000FF",
+	}
+
+	// Declaring map without key:value pair
+	//go will init map with it's empty/zero value
+	var colorMap map[string]string
+
+	//Yet another way to declare map
+	hex := make(map[string]string)
+
+	//how to populate a key/value in map
+	hex["#0000FF"] = "blue"
+
+	fmt.Println(colors)
+	fmt.Println(colorMap)
+	fmt.Println(hex)
+}
